8fa967dc1031b6c46b57e1a7a21c57a2ecab452c
Ian Mabon <ian_mabon@cate.org>
2020-02-06 11:53:54 -0800

    fourth commit, using struct and is weird

--- TableArrange.go
+++ TableArrange.go
@@ -11,19 +11,19 @@ import (
 	"time"
 )
 
-type Person struct {
-	Firstname string
-	Lastname  string
-	Table     string
-}
-
 func main() {
+	type Person struct {
+		Firstname string
+		Lastname  string
+		Table     []int
+	}
 	//Elliot's code modified
 	//empty tables
 	tableFill := []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	//used table array
 	usedTables := []int{}
-
-	var person []string
+	//defined person
+	var person []Person
 
 	csvFile, _ := os.Open("seating.csv")
 	reader := csv.NewReader(bufio.NewReader(csvFile))
@@ -34,48 +34,57 @@ func main() {
 		} else if error != nil {
 			log.Fatal(error)
 		}
-		//random seed
-		rand.Seed(time.Now().UnixNano())
+		person = append(person, Person{
+			Firstname: line[0],
+			Lastname:  line[1],
+		})
+	}
+
+	//random seed
+	rand.Seed(time.Now().UnixNano())
+
+	for studentNum, student := range person {
 		//random table number
 		var table = rand.Intn(30)
+		// table = append(table, Person.Table)
 		//table + 1
 		table++
-
-		var name = line[0] + " " + line[1]
-		person = append(person, name)
+		person[studentNum].Table = append(person[studentNum].Table, table)
 
 		if table == 29 {
 			//if the group is full, next table
-			if tableFill[table] < 31 {
-				fmt.Println(name, "KC")
+			if tableFill[table] < 7 {
+				fmt.Println(student, "KC")
 				tableFill[table]++
 			} else {
 				//else, add the integer assigned to the table to the usedTables slice.
-				fmt.Println(name, "KC")
+				fmt.Println(student, "KC")
 				usedTables = append(usedTables, table)
 			}
 		} else if table == 30 {
 			//if the group is full, next table
 			if tableFill[table] < 30 {
-				fmt.Println(name, "Waiter")
+				fmt.Println(student, "Waiter")
 				tableFill[table]++
 			} else {
 				//else, add the integer assigned to the table to the usedTables slice.
-				fmt.Println(name, "Waiter")
+				fmt.Println(student, "Waiter")
 				usedTables = append(usedTables, table)
 			}
 		} else {
 			if tableFill[table] < 8 {
-				fmt.Println(name, table)
+				//if the table is full, next table
+				fmt.Println(student, person[studentNum].Table)
 				tableFill[table]++
 			} else {
 				//else, add the integer assigned to the table to the usedTables slice.
 				// tableFill[table]++
-				fmt.Println(name, table)
+				fmt.Println(student, person[studentNum].Table)
 				usedTables = append(usedTables, table)
 			}
 		}
-		// fmt.Println(tableFill)
-		// fmt.Println(usedTables)
 	}
+
+	// fmt.Println(person)
+	// fmt.Println(usedTables)
 }
